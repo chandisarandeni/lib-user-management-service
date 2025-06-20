68bea008ee7216b4c416cccda64b8c8745a764be
chandisarandeni <chandisarandeni2003@gmail.com>
2025-06-20 21:14:06 +0530

    init : project with maven config 🚀

---
+++ src/test/java/com/sarasavi/lib_user_management_service/LibUserManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarasavi.lib_user_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibUserManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
