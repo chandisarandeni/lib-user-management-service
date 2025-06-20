647b4af6782de988406d6639210ac36a857a0bde
chandisarandeni <chandisarandeni2003@gmail.com>
2025-06-20 21:26:24 +0530

    feat: implement database config -> mysql 😎

--- src/main/java/com/sarasavi/lib_user_management_service/LibUserManagementServiceApplication.java
+++ src/main/java/com/sarasavi/lib_user_management_service/LibUserManagementServiceApplication.java
@@ -1,3 +1,9 @@
+// User Guide:
+// ***************************************************************************
+// **** Please make sure to follow the steps below to run the application ****
+// ***************************************************************************
+// Database creation script: CREATE DATABASE lib_user_management_db;
+
 package com.sarasavi.lib_user_management_service;
 
 import org.springframework.boot.SpringApplication;
