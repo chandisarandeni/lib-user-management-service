389b2b5a1e2a7248a2728588f842bf6a88ecfaf1
chandisarandeni <chandisarandeni2003@gmail.com>
2025-06-21 00:43:19 +0530

    feat: implement get member functions ✅

--- src/main/java/com/sarasavi/lib_user_management_service/controller/MemberController.java
+++ src/main/java/com/sarasavi/lib_user_management_service/controller/MemberController.java
@@ -1,14 +1,33 @@
 package com.sarasavi.lib_user_management_service.controller;
 
+import com.sarasavi.lib_user_management_service.dto.MemberDTO;
 import com.sarasavi.lib_user_management_service.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(path = "api/v1/")
 public class MemberController {
 
     @Autowired
     private MemberService memberService;
 
+    // -------- basic CRUD operations added here --------
 
+    // get all members
+    @GetMapping(path = "/members")
+    public List<MemberDTO> getMembers() {
+        return memberService.getAllMembers();
+    }
+
+    // get member by id
+    @GetMapping(path = "/members/{memberId}")
+    public MemberDTO getMemberById(@PathVariable("memberId") int memberId) {
+        return memberService.getMemberById(memberId);
+    }
 }
--- src/main/java/com/sarasavi/lib_user_management_service/service/MemberService.java
+++ src/main/java/com/sarasavi/lib_user_management_service/service/MemberService.java
@@ -1,9 +1,13 @@
 package com.sarasavi.lib_user_management_service.service;
 
+import com.sarasavi.lib_user_management_service.dto.MemberDTO;
+import com.sarasavi.lib_user_management_service.entity.Member;
 import com.sarasavi.lib_user_management_service.repository.MemberRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -14,5 +18,19 @@ public class MemberService {
     @Autowired
     private ModelMapper modelMapper;
 
+    // ----------- basic CRUD operations added here
 
+    // get all members
+    public List<MemberDTO> getAllMembers() {
+        return memberRepository.findAll().stream()
+                .map(member -> modelMapper.map(member, MemberDTO.class))
+                .toList();
+    }
+
+    // get member by id
+    public MemberDTO getMemberById(int memberId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new RuntimeException("Member not found"));
+        return modelMapper.map(member, MemberDTO.class);
+    }
 }
