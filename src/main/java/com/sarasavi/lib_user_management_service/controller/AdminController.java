07649d9212a5d265a6ed62954058a11359827192
chandisarandeni <chandisarandeni2003@gmail.com>
2025-06-23 21:37:39 +0530

    feat: implement get-all-admins ✅

--- src/main/java/com/sarasavi/lib_user_management_service/controller/AdminController.java
+++ src/main/java/com/sarasavi/lib_user_management_service/controller/AdminController.java
@@ -1,14 +1,24 @@
 package com.sarasavi.lib_user_management_service.controller;
 
+import com.sarasavi.lib_user_management_service.dto.AdminDTO;
 import com.sarasavi.lib_user_management_service.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(path = "/api/v1/")
 public class AdminController {
 
     @Autowired
     private AdminService adminService;
 
-
+    // get all admins
+    @GetMapping(path = "/admins")
+    public List<AdminDTO> getAdmins() {
+        return adminService.getAllAdmins();
+    }
 }
--- src/main/java/com/sarasavi/lib_user_management_service/service/AdminService.java
+++ src/main/java/com/sarasavi/lib_user_management_service/service/AdminService.java
@@ -1,9 +1,12 @@
 package com.sarasavi.lib_user_management_service.service;
 
+import com.sarasavi.lib_user_management_service.dto.AdminDTO;
 import com.sarasavi.lib_user_management_service.repository.AdminRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AdminService {
@@ -14,5 +17,10 @@ public class AdminService {
     @Autowired
     private ModelMapper modelMapper;
 
-
+    // get all admins
+    public List<AdminDTO> getAllAdmins() {
+        return adminRepository.findAll().stream()
+                .map(admin -> modelMapper.map(admin, AdminDTO.class))
+                .toList();
+    }
 }
